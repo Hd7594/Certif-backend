8c3183fe78dcb9a069e2f5dcef7db2ab9770a1f5
Saoul DImbanda <saouldimbanda@MacBook-Pro-de-Saoul.local>
2023-06-04 10:50:18 +0200

    add route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/test", (req, res) => {
+  res.json({ message: "test" });
+});
+
 app.listen(process.env.PORT, () => {
   console.log("Server started");
 });
